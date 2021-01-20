7a9991fd237e81c922e8004d0aaaaaebd554ed0d
Shawn Vosburg <shawn.vosburg@usherbrooke.ca>
2021-01-20 08:16:51 -0500

    Added tests to verify that SHA1 is same as the real git program

--- tests/unittests.hpp
+++ tests/unittests.hpp
@@ -8,3 +8,26 @@ void clearBuffer(char* buffer, int size)
 {
 	for(int i = 0; i < size; size++) buffer[i] = '\0';
 }
+
+
+string execSystemCommand(string command) {
+   char buffer[128];
+   string result = "";
+
+   // Open pipe to file
+   FILE* pipe = popen(command.c_str(), "r");
+   if (!pipe) {
+      return string("popen failed!");
+   }
+
+   // read till end of process:
+   while (!feof(pipe)) {
+
+      // use buffer to read and add to result
+      if (fgets(buffer, 128, pipe) != NULL)
+         result += buffer;
+   }
+
+   pclose(pipe);
+   return result;
+}
