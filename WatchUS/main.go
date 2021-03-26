8679b984ee8673ebd699f2771a4c92f67ad09cb4
Shawn Vosburg <shawn.vosburg@usherbrooke.ca>
2021-03-26 11:55:09 -0400

    WatchUS now takes a docker-compose as json data!

--- WatchUS/main.go
+++ WatchUS/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 	"os/exec"
 )
+
+type watchable struct {
+	File string `json:"file"`
+}
 
 func main() {
 	http.HandleFunc("/up", dockerComposeUp)
@@ -15,10 +22,31 @@ func main() {
 }
 
 func dockerComposeUp(writer http.ResponseWriter, reqest *http.Request) {
-	// Assumes the docker-compose.yml is in its /work directory
+	// Initialize empty toWatch
+	var toWatch watchable
+
+	// Parse request for the json which contains the file
+	body, _ := ioutil.ReadAll(reqest.Body)
+	errJSON := json.Unmarshal([]byte(body), &toWatch)
+	if errJSON != nil {
+		panic(errJSON)
+	}
+	defer reqest.Body.Close()
+
+	// Write the file to disk at /work/docker-compose.yml
+	file, err := os.Create("/work/docker-compose.yml")
+	file.WriteString(toWatch.File)
+	if err != nil {
+		fmt.Println("Creating docker-compose.yml file does not work!", err)
+	}
+
+	// With the docker-compose.yml file in the /work directory, we can now launch it!
 	// Pull necessary images
 	cmdArgs := []string{"-f", "/work/docker-compose.yml", "pull"}
-	_, err := exec.Command("docker-compose", cmdArgs...).Output()
+	_, err = exec.Command("docker-compose", cmdArgs...).Output()
+	if err != nil {
+		fmt.Println("docker-compose does not work!", err)
+	}
 
 	// Run in detach mode.
 	cmdArgs = []string{"-f", "/work/docker-compose.yml", "up", "-d", "--force-recreate"}
--- continuous-delivery/cd.py
+++ continuous-delivery/cd.py
@@ -13,15 +13,15 @@ DOT_MACHINES_PATH = pathlib.Path(__file__).absolute().parent.joinpath(".machines
 
 # 1. Load the list of workers
 with open(DOT_MACHINES_PATH,'r') as file:
-    machines = json.load(file)['machines']
+    machines = json.load(file)
 
-# 2. Send a GET requests to all watch us to the /up endpoints
-for machine in machines:
-    
-    # This is where the WatchUS is waiting for a signal
-    endpoint = f"http://{machine}:{WATCHUS_PORT}/up"
-
-    # Perform continuous delivery
-    urllib.request.urlopen(endpoint)
+# 2. Deliver DeployUS
+DeployUS_location = pathlib.Path(__file__).absolute().parent.joinpath("../DeployUS/docker-compose.yml")
+DeployUS_url = f"http://{machines['DeployUS']}:{WATCHUS_PORT}/up"
+with open(DeployUS_location) as compose_file:
+    DeployUS_file = compose_file.read()
+DeployUS_dict = {"file":DeployUS_file}
+DeployUS_json = json.dumps(DeployUS_dict).encode('utf-8')
+resp = urllib.request.urlopen(DeployUS_url, data=DeployUS_json)
 
 
